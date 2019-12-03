19ce7ef347292ee599d1370ff6ebb5f87aa894f4
Aaron Kim <akaaronkim@gmail.com>
2019-12-03 01:49:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,7 +10,7 @@ def directors_totals(nds)
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {}
 
-  nds.each do |directors_database|
+  nds.each do |directors_info|
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
@@ -20,5 +20,5 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+
 end
